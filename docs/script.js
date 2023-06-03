cfd3fd521ff17a784642bbb8e1488a557ee39deb
andouda <135394458+andouda@users.noreply.github.com>
2023-06-03 20:59:36 +0000

    トップページ更新5:59

---
+++ docs/script.js
@@ -0,0 +1,9 @@
+$(function(){
+  let color=0;
+  let colors=['lightyellow','lightgray','lightpink','lightblue'];
+  $("#button").on("click",function(){
+    if(color>colors.length)color=0;
+    $('body').css('background-color',colors[color]);
+    color++;
+  });
+});
