5d1cbed4a8a161f22d2e15e92e9f7ae7b8c0f0de
andouda <135394458+andouda@users.noreply.github.com>
2023-06-03 21:10:44 +0000

    更新6:10

--- docs/script.js
+++ docs/script.js
@@ -2,7 +2,7 @@ $(function(){
   let color=0;
   let colors=['lightyellow','lightgray','lightpink','lightblue'];
   $("#button").on("click",function(){
-    if(color>colors.length)color=0;
+    if(color>=colors.length)color=0;
     $('body').css('background-color',colors[color]);
     color++;
   });
